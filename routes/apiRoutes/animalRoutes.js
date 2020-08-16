1c561fb874e4b170c3ae531e3c36a1fb22fd9584
Waves <64664814+sanders43@users.noreply.github.com>
2020-08-16 17:03:24 -0500

    zookeepers form

---
+++ routes/apiRoutes/animalRoutes.js
@@ -0,0 +1,35 @@
+const { filterByQuery, findById, createNewAnimal, validateAnimal } = require("../../lib/animals");
+const { animals } = require("../../data/animals");
+const router = require('express').Router();
+
+router.get('/animals', (req, res) => {
+    let results = animals;
+    if (req.query) {
+      results = filterByQuery(req.query, results);
+    }
+    res.json(results);
+  });
+
+  router.get("/animals/:id", (req,res) => {
+    const result = findById(req.params.id, animals);
+    if (result) {
+        res.json(result);
+    } else {
+        res.send(404);
+    }
+});
+
+router.post("/animals", (req,res) => {
+    //req.body is where our incoming content will be
+    req.body.id = animals.length.toString();
+    if(!validateAnimal(req.body)) {
+        res.status(400).send("The animal is not properly formatted.");
+    } else {
+    // add animal to json file and animals array
+    const animal = createNewAnimal(req.body,animals);
+    res.json(animal);
+    }
+});
+
+
+module.exports = router;
